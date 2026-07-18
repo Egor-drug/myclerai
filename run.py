c277b2539c94c3ddef34d7fc5d8fb540341b2256
Egorka-drug <sashamorozov907@gmail.com>
2026-07-18 16:50:39 +0300

    Update run.py

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from aiogram import Bot,Dispatcher
 
 from config import TOKEN
 import asyncio
-from ChatComplete.app.handlers import router
+from app.handlers import router
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
